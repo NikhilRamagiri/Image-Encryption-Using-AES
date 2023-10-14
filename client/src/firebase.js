7b8a831fff7a35de5c9caef657000b311c26e750
NikhilRamagiri <64945684+NikhilRamagiri@users.noreply.github.com>
2023-10-14 21:59:00 +0530

    Update firebase.js

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -8,13 +8,13 @@ import "firebase/storage";
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyB6ALqmlGvT9kyuq2ChOhcoxvsubWQzQJU",
-  authDomain: "image-encryption-6a6a9.firebaseapp.com",
-  projectId: "image-encryption-6a6a9",
-  storageBucket: "image-encryption-6a6a9.appspot.com",
-  messagingSenderId: "240010401206",
-  appId: "1:240010401206:web:466055ef9cf0138a8b5d17",
-  measurementId: "G-TF7BNNYK1X"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 
 const app = initializeApp(firebaseConfig);
